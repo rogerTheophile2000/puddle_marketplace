bcc29206cf06f1498d07b94e8947d96b8c63e467
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-04-28 11:41:35 +0300

    adding authentication

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,16 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from . import views
-
+from .forms import LoginForm
 app_name = 'core'
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views. contact, name="contact"),
-    
+    path('signup/', views.signup, name="signup"),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name="login"),
 ]
